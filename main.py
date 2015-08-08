c4f88d929b076d34e099aaa085436f70430fe199
Santiago <santiago_cloud98@hotmail.com>
2015-08-08 14:56:14 -0500

    Funciones de las 3 apis

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/api_twitter')
+def twitter_api():
+    return 'twitter api'
+
+@app.route("/api_braintree")
+def braintree_api():
+	return "braintree api"
+
+@app.route("/api_facebook")
+def facebook_api():
+	return "facebook api"
+
 if __name__ == '__main__':
     app.run()
